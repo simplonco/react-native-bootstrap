b0d2e481ac580a211e750d4e7ea882e6105bba0b
saraguerric <sara.guerric@evergig.com>
2018-03-21 21:05:56 +0100

    Add : create-react-native-app bootstrap

---
+++ comp/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Header extends React.Component {
+
+  render () {
+    return (
+      <View>
+        <Text>Hello World !</Text>
+        <Text>Hello Paris !</Text>
+      </View>
+    )
+  }
+}
